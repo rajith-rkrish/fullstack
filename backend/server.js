a02f6ab9345b68a43cef282d626111e794aecacd
rajith-rkrish <rajith.radhakrishnan@jklucent.com>
2022-11-04 09:13:12 +0530

    commmit before authentication

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
-// import express from "express";
-// import cors from "cors";
-// import restaurants from "./api/restaurants.route.js";
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-// const app = express();
+connectDB();
 
-// app.use(cors());
-// app.use(express.json);
+const app = express();
 
-// app.use("/api/v1/restaurants", restaurants);
-// app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// export default app;
+app.use("/api/goals", require("./routes/goalRoutes"));
 
-console.log("Hello World");
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`server started on port ${port}`));
